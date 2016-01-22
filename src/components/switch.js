355370d7037c8368a142dcdff5d8ba31e42901db
Hanks <zhang.han113@zte.com.cn>
2016-01-22 15:39:48 +0800

    给 switch 组件添加 isChecked 接口

--- src/components/switch.js
+++ src/components/switch.js
@@ -3,10 +3,17 @@
 
     // 日期选择插件的构造函数
     function SwitchButton(options) {
+        this.$input = $('<input type="checkbox">');
         this.$el = $('<label class="switch">')
-            .append($('<input type="checkbox" checked="checked">'))
+            .append(this.$input)
             .append($('<span></span>'));
     }
+
+    _.extend(SwitchButton.prototype, {
+        isChecked: function() {
+            return this.$input.is(':checked');
+        },
+    });
 
     root.SwitchButton = SwitchButton;
 })(window)
