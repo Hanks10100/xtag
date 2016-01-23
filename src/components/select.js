7ad1daf6299ba41cb40a38ad2ed2a0f1ee0e93be
Hanks <673459756@qq.com>
2016-01-23 16:20:17 +0800

    给 select 添加基本的展开关闭事件

--- src/components/select.js
+++ src/components/select.js
@@ -4,8 +4,8 @@
     // 日期选择插件的构造函数
     function Select(options) {
         var self = this;
+        this.$root = $('body');
         this.$el = $('<div class="input-group ui-combobox"></div>');
-        this.$select = $('<select></select>');
         this.$option = $('<ul class="dropdown-list"></ul>');
 
         _.each(options.children, function(child) {
@@ -24,8 +24,39 @@
             $('<span class="input-group-addon clear-input-icon-right"></span>')
                 .append('<span class="glyphicon glyphicon-triangle-bottom"></span>')
         );
-        this.$el.append(this.$option);
+        // this.$el.append(this.$option);
+        this.bingEvents();
     }
+
+    _.extend(Select.prototype, {
+        bingEvents: function() {
+            var self = this;
+            this.$el.on('click', '.input-group-addon', function(event) {
+                event.stopPropagation();
+                self.toggleOptions();
+            });
+            this.$root.on('click', function() {
+                self.$option.detach();
+                self._optionsIsOpen = false;
+            });
+        },
+        toggleOptions: function(event) {
+            if (this._optionsIsOpen) {
+                this.$option.detach();
+                this._optionsIsOpen = false;
+            } else {
+                var offset = this.$el.offset();
+
+                this.$option.css('top', offset.top + this.$el.height() - 1)
+                    .css('left', offset.left)
+                    .width(this.$el.width())
+
+                this.$option.appendTo(this.$root);
+                this._optionsIsOpen = true;
+            }
+        },
+    });
+
 
     Framework.Select = Select;
 })(window, window.UED)
