0593a26fc02ee65c5a7a7d7468a10c4125a1b189
Hanks <673459756@qq.com>
2016-01-23 16:41:00 +0800

    添加与“启用”、“禁用” 相关的 mixin

--- src/shared/mixins.js
+++ src/shared/mixins.js
@@ -1,6 +1,21 @@
 ;(function(root, Framework){
     'use strict';
 
-    Framework.mixins = {};
+    // 与“启用”、“禁用” 相关的 函数
+    var availableMixin = {
+        setAvailable: function(value) {
+            this.$el.toggleClass('disabled', !value);
+            this.$input.prop('disabled', !value);
+            return this;
+        },
+        isEnabled:    function() { return this.$input.is(':enabled'); },
+        enable:       function() { return this.setAvailable(true); },
+        disable:      function() { return this.setAvailable(false); },
+        toggleEnable: function() { return this.setAvailable(!this.isEnabled()); },
+    };
+
+    Framework.mixins = {
+        availableMixin: availableMixin,
+    };
 
 })(window, window.UED)
