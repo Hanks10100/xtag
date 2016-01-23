9874bbb8fe6ed8a3e4f2b942ce83ef904db927e8
Hanks <673459756@qq.com>
2016-01-23 15:33:58 +0800

    向 select 传递 option 中的内容

--- src/components/select.js
+++ src/components/select.js
@@ -3,7 +3,21 @@
 
     // 日期选择插件的构造函数
     function Select(options) {
-        this.$el = $('<select></select>');
+        var self = this;
+        this.$select = $('<select></select>');
+        this.$option = [];
+
+        _.each(options.children, function(child) {
+            // console.dir(child);
+            self.$option.push(
+                $('<option></option>')
+                    .attr('value', child.getAttribute('value'))
+                    .html(child.innerHTML)
+            );
+        });
+
+        this.$select.append(this.$option);
+        this.$el = this.$select;
     }
 
     Framework.Select = Select;
