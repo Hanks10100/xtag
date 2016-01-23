f4d3e4f60c877a77468554a461f55dcca842b6f5
Hanks <673459756@qq.com>
2016-01-23 15:49:55 +0800

    模拟 Fish Combobox 组件的 DOM 结构

--- src/components/select.js
+++ src/components/select.js
@@ -4,20 +4,27 @@
     // 日期选择插件的构造函数
     function Select(options) {
         var self = this;
+        this.$el = $('<div class="input-group ui-combobox"></div>');
         this.$select = $('<select></select>');
-        this.$option = [];
+        this.$option = $('<ul class="dropdown-list"></ul>');
 
         _.each(options.children, function(child) {
             // console.dir(child);
-            self.$option.push(
-                $('<option></option>')
+            self.$option.append(
+                $('<li></li>')
                     .attr('value', child.getAttribute('value'))
                     .html(child.innerHTML)
             );
         });
 
-        this.$select.append(this.$option);
-        this.$el = this.$select;
+
+        // 模拟 Fish Combobox 组件的 DOM 结构
+        this.$el.append(
+            $('<input type="text" autocomplete="off" class="form-control">'),
+            $('<span class="input-group-addon clear-input-icon-right"></span>')
+                .append('<span class="glyphicon glyphicon-triangle-bottom"></span>')
+        );
+        this.$el.append(this.$option);
     }
 
     Framework.Select = Select;
