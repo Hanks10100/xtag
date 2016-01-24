a85060019c44fe18b463cdf08c683ec10b47b56e
Hanks <673459756@qq.com>
2016-01-24 20:40:41 +0800

    初始化 tabs DOM 结构

--- src/components/tabpanel.js
+++ src/components/tabpanel.js
@@ -3,10 +3,41 @@
 
     // Tab 页签
     function TabPanel(options) {
-        this.$el = $('<div class="tabs-panel"></div>');
+        options || (options = {});
+        var self = this;
+        this.activeTab = 0;
+        this.tabs = _.map(options.children, function(group, index) {
+            if (group.getAttribute('default')) self.activeTab = index;
+            return {
+                trigger: _.first(group.getElementsByTagName('trigger')),
+                target: _.first(group.getElementsByTagName('target')),
+            }
+        });
+        this.initElement();
     }
 
+
+
     _.extend(TabPanel.prototype, {
+        initElement: function() {
+            var self = this;
+            this.$trigger = $('<ul class="tabs-nav"></ul>');
+            this.$targets = $('<div class="tabs-content"></div>');
+            _.each(this.tabs, function(group, index) {
+                self.$trigger.append(
+                    $('<li class="tabs-nav-cell"></li>')
+                        .toggleClass('active', self.activeTab === index)
+                        .append($('<span></span>').html(group.trigger.innerHTML))
+                    );
+                self.$targets.append(
+                    $('<div class="tabs-panel"></div>')
+                        .toggle(self.activeTab === index)
+                        .html(group.target.innerHTML)
+                );
+            });
+            this.$el = $('<div class="ued-tabs"></div>').append(this.$trigger, this.$targets);
+            return this.$el;
+        },
         afterMount: function() {
         }
     });
