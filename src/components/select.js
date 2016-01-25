f23c85ca29028fb1c8e75e62978d9ca503082c0d
Hanks <zhang.han113@zte.com.cn>
2016-01-25 16:40:14 +0800

    select 组件添加 initElement 方法

--- src/components/select.js
+++ src/components/select.js
@@ -2,36 +2,40 @@
     'use strict';
 
     // 日期选择插件的构造函数
-    function Select(options) {
-        var self = this;
+    function Select() {
         this.$root = $('body');
 
-        // 模拟 Fish Combobox 组件的 DOM 结构
-        this.$el = $('<div class="input-group ui-combobox"></div>');
-        this.$input = $('<input type="text" autocomplete="off" class="form-control">');
-        this.$button = $('<span class="input-group-addon clear-input-icon-right"></span>')
-                .append('<span class="glyphicon glyphicon-triangle-bottom"></span>');
-        this.$el.append(this.$input, this.$button);
-
-        this._options = [];
-        this.$options = $('<ul class="dropdown-list"></ul>');
-        _.each(options.children, function(child, index) {
-            // console.dir(child);
-            var value = child.getAttribute('value');
-            self._options.push({ value: value, name: child.innerHTML });
-            self.$options.append(
-                $('<li></li>').attr('index', index)
-                    .data('value', value)
-                    .html(child.innerHTML)
-            );
-        });
-
-        // this.$el.append(this.$options);
+        this.initElement.apply(this, arguments);
         this.bingEvents();
     }
 
     _.extend(Select.prototype, {
         type: 'Select',
+        initElement: function(options) {
+            var self = this;
+
+            // 模拟 Fish Combobox 组件的 DOM 结构
+            this.$el = $('<div class="input-group ui-combobox"></div>');
+            this.$input = $('<input type="text" autocomplete="off" class="form-control">');
+            this.$button = $('<span class="input-group-addon clear-input-icon-right"></span>')
+                    .append('<span class="glyphicon glyphicon-triangle-bottom"></span>');
+            this.$el.append(this.$input, this.$button);
+
+            this._options = [];
+            this.$options = $('<ul class="dropdown-list"></ul>');
+            _.each(options.children, function(child, index) {
+                // console.dir(child);
+                var value = child.getAttribute('value');
+                self._options.push({ value: value, name: child.innerHTML });
+                self.$options.append(
+                    $('<li></li>').attr('index', index)
+                        .data('value', value)
+                        .html(child.innerHTML)
+                );
+            });
+
+            return this.$el;
+        },
         bingEvents: function() {
             var self = this;
             this.$el.on('click', '.input-group-addon', function(event) {
