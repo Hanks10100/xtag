44b0823f6e0b87a8bf90d8eaea471a8e746bbd49
Hanks <673459756@qq.com>
2016-01-30 02:08:43 +0800

    修改主页面的测试代码

--- demo/test.js
+++ demo/test.js
@@ -1,44 +1,12 @@
-// console.log('run test');
 define(['hbs!LayoutTpl'], function(LayoutTpl) {
     // console.log('tpl:', LayoutTpl())
-
-    // XTag.connectToFish(fish);
-    // XTag.connectToFish(fish, { enableView: true });
     // console.log(XTag);
 
     fish.View.configure({ manage: true });
     var DemoView = fish.View.extend({
         el: $('#pageRoot'),
         template: LayoutTpl,
-        initialize: function() {
-            // this.setView('#icheck', new XTag.Checkbox());
-        },
-        getTableOption: function() {
-            return {
-                head: [
-                    { text: '- A -' },
-                    { text: '- B -' },
-                    { text: '- C -' }
-                ],
-                body: [
-                    [
-                        { text: 'AAA' },
-                        { text: 'BBB' },
-                        { text: 'CCC' }
-                    ], [
-                        { text: 'A-1' },
-                        { text: 'B-1' },
-                        { text: 'C-1' }
-                    ], [
-                        { text: 'A-2' },
-                        { text: 'B-2' },
-                        { text: 'C-2' }
-                    ]
-                ]
-            };
-        },
         afterRender: function() {
-            var self = this;
             console.log('after render', this);
             window.switcher = this.switcher;
             window.checkbox = this.checkbox;
@@ -49,17 +17,13 @@ define(['hbs!LayoutTpl'], function(LayoutTpl) {
             window.tabSwitcher = this.tabSwitcher;
 
             // 绑定 change 事件
-            // this.switcher.onChange(function(event) {
-            //     // 在 onChange 的回调函数中，this 指向 switcher 实例
-            //     console.log('%cswitcher:', 'color:green', this.isChecked());
-            // });
-            // this.checkbox.onChange(function() {
-            //     console.log('%ccheckbox:', 'color:blue', self.checkbox.isChecked());
-            // });
-
-            // tabSwitcher.onChange(function() { table.$el.toggle(this.value) });
-            // switcher.onChange(function() { tab.activeTab = (tab.activeTab + 1)%3 });
-            // checkbox.onChange(function() { switcher.value = this.value });
+            this.switcher.onChange(function(event) {
+                // 在 onChange 的回调函数中，this 指向 switcher 实例
+                console.log('%cswitcher:', 'color:green', this.value);
+            });
+            this.checkbox.onChange(function() {
+                console.log('%ccheckbox:', 'color:blue', this.isChecked());
+            });
         }
     });
 
