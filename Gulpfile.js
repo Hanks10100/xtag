a1290b8e7ad0553a751fb1aec7288a2c7e0af9d7
Hanks <673459756@qq.com>
2016-01-30 14:42:49 +0800

    添加基本的打包合并命令

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,1 +1,15 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+var filePath = [
+    'src/shared/*.js',
+    'src/components/*.js',
+    'src/compile.js',
+    'src/connect.js',
+];
+
+gulp.task('script', function() {
+  return gulp.src(filePath)
+    .pipe(concat('xtag.js'))
+    .pipe(gulp.dest('dist'));
+});
