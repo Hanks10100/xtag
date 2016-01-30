aaddda2728b3d6cd972c0db61c8c5c497c1e6389
Hanks <673459756@qq.com>
2016-01-30 14:56:48 +0800

    配置 gulp 编译流程

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
+var del = require('del');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
 
 var filePath = [
     'src/shared/*.js',
@@ -8,8 +12,22 @@ var filePath = [
     'src/connect.js',
 ];
 
+gulp.task('clean', function() {
+  return del('./dist');
+});
+
 gulp.task('script', function() {
   return gulp.src(filePath)
+    .pipe(sourcemaps.init())
     .pipe(concat('xtag.js'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('script');
+    gulp.watch('src/**/*.js', ['script']);
 });
