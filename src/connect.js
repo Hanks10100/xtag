d41a778ea226e53855bce0a2d001b65ac29c967a
Hanks <673459756@qq.com>
2016-01-30 17:21:18 +0800

    connect 文件添加默认参数

--- src/connect.js
+++ src/connect.js
@@ -1,27 +1,24 @@
 import { compile } from './compile';
 import { preCompile } from './shared/staticTags';
 
-export function connectToFish(fish) {
-    var fish = fish || root.fish;
+export function connectToFish(fish = window.fish) {
     if (!fish) return;
 
     // 可以渲染静态的自定义标签
     customHandlebarsCompile();
 
     // 将自定义标签的编译过程插入到 fish 默认的渲染流程中
-    var originalRender = fish.View.prototype.__render;
-    _.extend(fish.View.prototype, {
-        __render: function() {
-            var res = originalRender.apply(this, arguments);
-            compile(this);
-            return res;
-        }
-    });
+    const originalRender = fish.View.prototype.__render;
+    fish.View.prototype.__render = function() {
+        var res = originalRender.apply(this, arguments);
+        compile(this);
+        return res;
+    }
 }
 
 // 自定义 Handlebars 的编译函数
 function customHandlebarsCompile() {
-    var HandlebarsCompile = Handlebars.compile;
+    const HandlebarsCompile = Handlebars.compile;
     Handlebars.compile = function(template, options) {
         var tpl = preCompile(template);
         return HandlebarsCompile(tpl, options);
