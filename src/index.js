17de0d5a00f5ddc46efe3aa2793cb484cd595538
Hanks <673459756@qq.com>
2016-01-30 17:11:11 +0800

    减少 XTag 对外暴露的接口

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-import utils from './shared/utils';
-import mixins from './shared/mixins';
-import components from './components/index';
-import { preCompile, registerTag, unregisterTag } from './shared/staticTags';
-import { compile, compileElement } from './compile';
+import { registerTag, unregisterTag } from './shared/staticTags';
+import { compile } from './compile';
 import { connectToFish } from './connect';
-
 
 connectToFish(fish);
 
 const XTag = {
-    utils,
-    mixins,
-    components,
-    preCompile,
     registerTag,
     unregisterTag,
     compile,
-    compileElement,
     connectToFish,
 };
 
