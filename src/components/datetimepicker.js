55f291228bd6f844d8bee20ec3a8326f8953df18
Hanks <673459756@qq.com>
2016-01-30 23:13:52 +0800

    将 Datetimepicker 转为 ES6

--- src/components/datetimepicker.js
+++ src/components/datetimepicker.js
@@ -3,8 +3,8 @@ import mixins from '../shared/mixins';
 
 // 日期选择插件的构造函数
 export class Datetimepicker {
-    constructor(options) {
-        this.options = options || {};
+    constructor(options = {}) {
+        this._options = options;
         this.$el = $('<div class="input-group"></div>').attr('data-type', this.type);
         this.$input = $('<input type="text" class="form-control">');
         this.$el.html(this.$input);
@@ -13,20 +13,17 @@ export class Datetimepicker {
     }
 
     mapMethods() {
-        var self = this;
-        var $picker = this.$input;
-        var methods = [
+        const $picker = this.$input;
+        const methods = [
             'destroy', 'disable', 'enable', 'getDate', 'setDate', 'hide', 'show',
             'value', 'setEndDate', 'setStartDate', 'option', 'setDatesDisabled',
             'widget', 'setDaysOfWeekDisabled', 'setDaysOfWeekHighlighted'
         ];
-        _.each(methods, function(method) {
-            self[method] = _.bind($picker.datetimepicker, $picker, method);
-        });
+        _.each(methods, method => this[method] = _.bind($picker.datetimepicker, $picker, method));
     }
 
     afterMount() {
-        this.$input.datetimepicker(this.options);
+        this.$input.datetimepicker(this._options);
     }
 }
 
