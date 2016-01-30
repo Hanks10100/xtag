fc5f8d386113d02974ff1247bfa049615dfae2f5
Hanks <673459756@qq.com>
2016-01-30 23:49:30 +0800

    将 tabs 转为 ES6

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,100 +1,91 @@
 import mixins from '../shared/mixins';
 import { compileElement } from '../compile';
 
-
-// Tab 页签
-export function Tabs() {
-    this.initialize(...arguments);
-    this.initElement(...arguments);
-    this.bindEvents();
-}
-
 function isValidateDirection(direction) {
     if (!direction || !_.isString(direction)) return false;
-    var dir = direction.toLowerCase();
+    const dir = direction.toLowerCase();
     if (_.indexOf(['left', 'right', 'top', 'bottom'], dir) === -1) return false;
     return dir;
 }
 
-_.extend(Tabs.prototype, {
-    type: 'Tabs',
+// Tab 页签
+export class Tabs {
+    constructor() {
+        this.initialize(...arguments);
+        this.initElement(...arguments);
+        this.bindEvents();
+    }
 
-    initialize: function(options, configs) {
-        options || (options = {});
-        configs || (configs = {});
-        var self = this;
+    initialize(options = {}, configs = {}) {
 
         this._scope = configs.scope;
 
         // 定义影子变量，可在 getter/setter 中绑定额外操作
         this.defineShadowValues({
-            value:     { get: function()    { return this.tabs[this.activeTab] } },
-            align:     { set: function(dir) { return this.alignTo(dir).align } },
-            activeTab: { set: function(idx) { return this.switchTo(idx).activeTab } }
+            value:     { get: ()  => this.tabs[this.activeTab]    },
+            align:     { set: dir => this.alignTo(dir).align      },
+            activeTab: { set: idx => this.switchTo(idx).activeTab }
         });
 
-        this.tabs = _.map(options.children, function(group, index) {
+        this.tabs = _.map(options.children, (group, index) => {
             if (group.getAttribute('active')) {
-                self.setShadowValue('activeTab', index);
+                this.setShadowValue('activeTab', index);
             }
             return {
-                index: index,
+                index,
                 trigger: _.first(group.getElementsByTagName('trigger')),
                 target: _.first(group.getElementsByTagName('target')),
             }
         });
 
         return this;
-    },
+    }
 
-    initElement: function(options, configs) {
-        var self = this;
+    initElement(options = {}, configs = {}) {
         this.$el = $('<div class="ued-tabs"></div>').attr('data-type', this.type);
         this.el = this.$el[0];
 
         this.$navList = $('<ul class="tabs-nav-list"></ul>');
         this.$contents = $('<div class="tabs-content"></div>');
-        _.each(this.tabs, function(group, index) {
+        _.each(this.tabs, (group, index) => {
             group.title = group.trigger.innerHTML;
             group.trigger = $('<li class="tabs-nav-cell"></li>')
                 .data('index', index)
                 .attr('title', group.title)
-                .toggleClass('active', self.activeTab === index)
-                .append($('<span></span>').html(group.title))
+                .toggleClass('active', this.activeTab === index)
+                .append(`<span>${group.title}</span>`)
 
             group.panel = group.target.innerHTML;
             group.target = $('<section class="tabs-panel"></section>')
                 .data('index', index)
-                .toggle(self.activeTab === index)
+                .toggle(this.activeTab === index)
                 .html(group.panel)
 
-            self.$navList.append(group.trigger);
-            self.$contents.append(group.target);
+            this.$navList.append(group.trigger);
+            this.$contents.append(group.target);
         });
         this.$navs = $('<nav class="tabs-nav"></nav>').append(this.$navList);
 
         this.alignTo(options.align || 'top');
         return this.$el;
-    },
+    }
 
-    bindEvents: function() {
-        var self = this;
-        this.$navs.on('click', '.tabs-nav-cell', function(event) {
-            self.switchTo($(event.currentTarget).data('index'));
+    bindEvents() {
+        this.$navs.on('click', '.tabs-nav-cell', event => {
+            this.switchTo($(event.currentTarget).data('index'));
         });
 
         // 创建属性监听函数，自动更新 align 的值
-        this.observeAttributes(['align'], function(attr, mutation, oldValue) {
-            var dir = isValidateDirection(mutation.target.align);
-            dir ? this.alignTo(dir) : (mutation.target.align = oldValue);
+        this.observeAttributes(['align'], (attr, { target }, oldValue) => {
+            const dir = isValidateDirection(target.align);
+            dir ? this.alignTo(dir) : (target.align = oldValue);
         });
 
         return this;
-    },
+    }
 
-    alignTo: function(direction) {
-        var dir = isValidateDirection(direction);
-
+    alignTo(direction) {
+        const dir = isValidateDirection(direction);
         if (!dir || (dir === this.align)) return this;
 
         switch (dir) {
@@ -118,29 +109,27 @@ _.extend(Tabs.prototype, {
         this.setShadowValue('align', dir);
         this.adjustLayout();
         return this;
-    },
+    }
 
-    switchTo: function(index) {
+    switchTo(index) {
         if (!_.isNumber(index)) return this;
         index = parseInt(index + this.tabs.length, 10) % this.tabs.length;
-        if (this.isEnabled()) {
 
-            if (this.activeTab !== index) {
-                var $cell = this.$navs.find('.tabs-nav-cell:nth-child('+(index+1)+')');
-                var $target = this.$contents.find('.tabs-panel:nth-child('+(index+1)+')');
-                $cell.addClass('active').siblings().removeClass('active');
-                $target.show().siblings().hide();
+        if (this.isEnabled() && (this.activeTab !== index)) {
+            this.$navs.find(`.tabs-nav-cell:nth-child(${index+1})`)
+                .addClass('active').siblings().removeClass('active');
 
-                this.setShadowValue('activeTab', index);
+            this.$contents.find(`.tabs-panel:nth-child(${index+1})`)
+                .show().siblings().hide();
 
-                this.trigger('change');
-            }
+            this.setShadowValue('activeTab', index);
+            this.trigger('change');
         }
         return this;
-    },
+    }
 
-    afterMount: function() {
-        var count = this.$el.parents('.ued-tabs').length + 1;
+    afterMount() {
+        const count = this.$el.parents('.ued-tabs').length + 1;
         this.$el.attr('level', count).data('level', count);
         switch (count) {
             case 1: this.$el.addClass('root-tab');   break;
@@ -157,34 +146,36 @@ _.extend(Tabs.prototype, {
         // 编译 Tabs 面板中的内容
         compileElement(this.$contents, this._scope);
         this.adjustLayout();
-    },
+    }
 
-    adjustLayout: function() {
+    adjustLayout() {
         if (!this.$el.is(':visible')) return this;
 
-        var el = {
+        const el = {
             width: this.$el.outerWidth(),
             height: this.$el.outerHeight()
         };
-        var nav = {
+        const nav = {
             width: this.$navs.outerWidth(),
             height: this.$navs.outerHeight()
         };
-        var panel = {
+        const panel = {
             width: this.$contents.outerWidth(),
             height: this.$contents.outerHeight()
         };
         if (this.tabStyle === 'vertical') {
-            var maxHeight = Math.max(el.height, nav.height, panel.height);
+            const maxHeight = Math.max(el.height, nav.height, panel.height);
             this.$el.height(maxHeight);
         }
         return this;
-    },
+    }
 
-    getCurrentTab: function() {
+    getCurrentTab() {
         return this.tabs[this.activeTab];
-    },
-});
+    }
+}
+
+Tabs.prototype.type = 'Tabs';
 
 // 添加启用和禁用功能
 _.extend(Tabs.prototype, mixins.authorize);
